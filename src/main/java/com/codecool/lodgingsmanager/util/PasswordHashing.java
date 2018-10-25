497257a7f36cfb28e87009e9b4575de9263d1768
attilaKincsei <akincsei@gmail.com>
2018-10-25 11:27:03 +0200

    - user login bugs removed: wrong email and password does not break program

--- src/main/java/com/codecool/lodgingsmanager/util/PasswordHashing.java
+++ src/main/java/com/codecool/lodgingsmanager/util/PasswordHashing.java
@@ -5,6 +5,8 @@ import com.codecool.lodgingsmanager.dao.implementation.database.UserDaoDb;
 import com.codecool.lodgingsmanager.model.User;
 import org.mindrot.jbcrypt.BCrypt;
 
+import javax.persistence.NoResultException;
+
 public class PasswordHashing {
 
 
@@ -12,7 +14,7 @@ public class PasswordHashing {
         return BCrypt.hashpw(password, BCrypt.gensalt(12));
     }
 
-    public static User checkPassword(String candidate, String email) {
+    public static User checkPassword(String candidate, String email) throws NoResultException {
         UserDao userDataManager = new UserDaoDb();
         User user = userDataManager.findIdBy(email);
         String hashed = user.getPasswordHash();
