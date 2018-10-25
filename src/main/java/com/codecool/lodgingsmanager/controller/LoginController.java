0f432e0507b1cf2a0c23d657125bb33598c43a5c
attilaKincsei <akincsei@gmail.com>
2018-10-25 11:27:03 +0200

    - user login bugs removed: wrong email and password does not break program

--- src/main/java/com/codecool/lodgingsmanager/controller/LoginController.java
+++ src/main/java/com/codecool/lodgingsmanager/controller/LoginController.java
@@ -9,37 +9,48 @@ import com.codecool.lodgingsmanager.util.UserDataField;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.persistence.NoResultException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/login"})
+@WebServlet(urlPatterns = {"/login", "/login-incorrect"})
 public class LoginController extends HttpServlet {
 
     private UserDao userDataManager = new UserDaoDb();
+    private String guestEmail = "guest@fakedomain.com";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        HttpSession session = request.getSession(false);
-
+        HttpSession oldSession = request.getSession(true);
 
         String errorMessage;
 
-        WebContext context = new WebContext(request, response, request.getServletContext());
-
-        if (session != null && session.getAttribute(UserDataField.EMAIL_ADDRESS.getInputString()) != null) {
-            String emailAddress = String.valueOf(session.getAttribute(UserDataField.EMAIL_ADDRESS.getInputString()));
-            session.setAttribute(UserDataField.EMAIL_ADDRESS.getInputString(), "guest@fakedomain.com");
-            errorMessage = "You have just logged out with email address: " + emailAddress + ". Register or log in, please";
-        } else {
+        String loggedInUserEmail = (String) oldSession.getAttribute(UserDataField.EMAIL_ADDRESS.getInputString());
+        if (loggedInUserEmail == null) {
             errorMessage = "You are not logged in. Register or log in, please";
+        } else {
+            oldSession.invalidate();
+            String requestPath = request.getServletPath();
+            String requestMethod = request.getMethod();
+            if (requestPath.equals("/login-incorrect")) {
+                errorMessage = "Incorrect user name or password";
+            } else if (loggedInUserEmail.equals(guestEmail)) {
+                errorMessage = "You are not logged in. Register or log in, please";
+            } else {
+                errorMessage = "You are logged out";
+            }
         }
 
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-        User guestUser = userDataManager.findIdBy("guest@fakedomain.com");
-        context.setVariable("userData", guestUser);
+        HttpSession newSession = request.getSession();
+        newSession.setAttribute(UserDataField.EMAIL_ADDRESS.getInputString(), guestEmail);
 
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        User guestUser = userDataManager.findIdBy(guestEmail);
+
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        context.setVariable("userData", guestUser);
         context.setVariable("errorMessage", errorMessage);
         engine.process("login.html", context, response.getWriter());
         }
@@ -47,34 +58,26 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        HttpSession oldSession = request.getSession(false);
-
-        if (oldSession != null) {
-            oldSession.invalidate();
-        }
-
         String email = request.getParameter(UserDataField.EMAIL_ADDRESS.getInputString());
         String password = request.getParameter(UserDataField.PASSWORD.getInputString());
 
-        User mightBeUser = PasswordHashing.checkPassword(password, email);
-        if (mightBeUser == null) {
-            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-            WebContext context = new WebContext(request, response, request.getServletContext());
-            context.setVariable("errorMessage", "The email or password is incorrect.");
-            engine.process("login.html", context, response.getWriter());
-        } else {
+        try {
+            User mightBeUser = PasswordHashing.checkPassword(password, email);
             HttpSession newSession = request.getSession(true);
             newSession.setAttribute(UserDataField.EMAIL_ADDRESS.getInputString(), mightBeUser.getEmail());
             newSession.setMaxInactiveInterval(30*60);
-
 
             Cookie userEmail = new Cookie(UserDataField.EMAIL_ADDRESS.getInputString(), mightBeUser.getEmail());
             userEmail.setMaxAge(30*60);
             response.addCookie(userEmail);
 
-
             response.sendRedirect("/index");
 
+        } catch (NoResultException | NullPointerException ex) {
+            System.out.println("User not in database"); // todo: change it to logger
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+            WebContext context = new WebContext(request, response, request.getServletContext());
+            response.sendRedirect("/login-incorrect");
         }
 
     }
